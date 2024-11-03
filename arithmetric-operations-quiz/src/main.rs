118bd620895459bd21ffc17b602a11df412dc449
sawady1203 <sawady.dev@gmail.com>
2024-11-03 16:01:27 +0900

    section3-3/減算の問題

--- arithmetric-operations-quiz/src/main.rs
+++ arithmetric-operations-quiz/src/main.rs
@@ -6,9 +6,7 @@ fn main() {
 
     // 標準入力から1行取得し、ans_inputに代入する
     std::io::stdin().read_line(&mut ans_intput).unwrap();
-
-    let ans_input = ans_intput.trim().parse::<u32>().unwrap();
-
+    let ans_input = ans_intput.trim().parse::<i32>().unwrap();
     dbg!(ans_input);
 
     if dbg!(ans_input == 1 + 1) {
@@ -16,4 +14,22 @@ fn main() {
     } else {
         println!("不正解！")
     }
+
+    println!("1 - 4 = ??");
+    println!("?? の値を入兎力してください:");
+    // ユーザーからの回答を保持する変数
+    let mut ans_intput = String::new();
+
+    // 標準入力から1行取得し、ans_inputに代入する
+    std::io::stdin().read_line(&mut ans_intput).unwrap();
+    let ans_input = ans_intput.trim().parse::<i32>().unwrap();
+    dbg!(ans_input);
+
+    if dbg!(ans_input == 1 - 4) {
+        println!("正解！")
+    } else {
+        println!("不正解！")
+    }
+    println!("i32が扱えるデータ範囲:{} ~ {}", i32::MIN, i32::MAX);
+    println!("u32が扱えるデータ範囲:{} ~ {}", u32::MIN, u32::MAX);
 }
