c30f8691332a02ce2fd0b50fca1ec3c50161ea58
sawady1203 <sawady.dev@gmail.com>
2024-11-03 23:16:05 +0900

    section5-2:関数を使わない実装

--- dentaku/src/main.rs
+++ dentaku/src/main.rs
@@ -1,3 +1,30 @@
+use std::io::stdin;
+
 fn main() {
-    println!("Hello, world!");
+    for line in stdin().lines() {
+        // 1行読み取って空行なら終了
+        let line = line.unwrap();
+        if line.is_empty() {
+            break;
+        }
+        // スペースで分割
+        let tokens: Vec<&str> = line.split(char::is_whitespace).collect();
+        println!("{:?}", tokens);
+        // 式の計算
+        let left: f64 = tokens[0].parse().unwrap();
+        let right: f64 = tokens[2].parse().unwrap();
+        let result = match tokens[1] {
+            "+" => left + right,
+            "-" => left - right,
+            "*" => left * right,
+            "/" => left / right,
+            _ => {
+                // 入力が正しいならここにはこない
+                unreachable!()
+            }
+        };
+
+        // 結果の表示
+        println!(" => {}", result)
+    }
 }
