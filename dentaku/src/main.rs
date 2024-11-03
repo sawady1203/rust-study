588ddf51b9300406765d5223268661b227ed6667
sawady1203 <sawady.dev@gmail.com>
2024-11-03 23:21:49 +0900

    section5-2:関数を使うメリット

--- dentaku/src/main.rs
+++ dentaku/src/main.rs
@@ -14,10 +14,10 @@ fn main() {
         let left: f64 = tokens[0].parse().unwrap();
         let right: f64 = tokens[2].parse().unwrap();
         let result = match tokens[1] {
-            "+" => left + right,
-            "-" => left - right,
-            "*" => left * right,
-            "/" => left / right,
+            "+" => add_values(left, right),
+            "-" => subtract_values(left, right),
+            "*" => multiply_values(left, right),
+            "/" => divide_values(left, right),
             _ => {
                 // 入力が正しいならここにはこない
                 unreachable!()
@@ -25,6 +25,26 @@ fn main() {
         };
 
         // 結果の表示
-        println!(" => {}", result)
+        print_value(result);
     }
+}
+
+fn print_value(value: f64) {
+    println!(" => {}", value);
+}
+
+fn add_values(left: f64, right: f64) -> f64 {
+    left + right
+}
+
+fn subtract_values(left: f64, right: f64) -> f64 {
+    left - right
+}
+
+fn multiply_values(left: f64, right: f64) -> f64 {
+    left * right
+}
+
+fn divide_values(left: f64, right: f64) -> f64 {
+    left / right
 }
