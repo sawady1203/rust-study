a75364891e9c32fc775c55f457c61c1a78835381
sawady1203 <sawady.dev@gmail.com>
2024-11-03 16:18:05 +0900

    section3-4/for in / while文

--- arithmetric-operations-quiz/src/main.rs
+++ arithmetric-operations-quiz/src/main.rs
@@ -1,39 +1,48 @@
 use rand::Rng;
 
 fn main() {
-    let op1 = rand::thread_rng().gen_range(0..100);
-    let op2 = rand::thread_rng().gen_range(0..100);
-    println!("{} + {} = ??", op1, op2);
-    println!("?? の値を入兎力してください:");
-    // ユーザーからの回答を保持する変数
-    let mut ans_intput = String::new();
+    let mut num_of_correct = 0; // 正解の回数を数える
+    while num_of_correct < 3 {
+        let op1 = rand::thread_rng().gen_range(0..100);
+        let op2 = rand::thread_rng().gen_range(0..100);
+        println!("{} + {} = ??", op1, op2);
+        println!("?? の値を入兎力してください:");
+        // ユーザーからの回答を保持する変数
+        let mut ans_intput = String::new();
 
-    // 標準入力から1行取得し、ans_inputに代入する
-    std::io::stdin().read_line(&mut ans_intput).unwrap();
-    let ans_input = ans_intput.trim().parse::<i32>().unwrap();
-    dbg!(ans_input);
+        // 標準入力から1行取得し、ans_inputに代入する
+        std::io::stdin().read_line(&mut ans_intput).unwrap();
+        let ans_input = ans_intput.trim().parse::<i32>().unwrap();
+        dbg!(ans_input);
 
-    if dbg!(ans_input == op1 + op2) {
-        println!("正解！")
-    } else {
-        println!("不正解！")
+        if dbg!(ans_input == op1 + op2) {
+            println!("正解！");
+            num_of_correct += 1; // 正解したら1回足す
+            if num_of_correct >= 3 {
+                break;
+            }; //3回正解したら抜ける
+        } else {
+            println!("不正解！")
+        }
+
+        let op1 = rand::thread_rng().gen_range(0..100);
+        let op2 = rand::thread_rng().gen_range(0..100);
+        println!("{} - {} = ??", op1, op2);
+        println!("?? の値を入兎力してください:");
+        // ユーザーからの回答を保持する変数
+        let mut ans_intput = String::new();
+
+        // 標準入力から1行取得し、ans_inputに代入する
+        std::io::stdin().read_line(&mut ans_intput).unwrap();
+        let ans_input = ans_intput.trim().parse::<i32>().unwrap();
+        dbg!(ans_input);
+
+        if dbg!(ans_input == op1 - op2) {
+            println!("正解！");
+            num_of_correct += 1; // 正解したら1回足す
+        } else {
+            println!("不正解！")
+        }
     }
-
-    let op1 = rand::thread_rng().gen_range(0..100);
-    let op2 = rand::thread_rng().gen_range(0..100);
-    println!("{} - {} = ??", op1, op2);
-    println!("?? の値を入兎力してください:");
-    // ユーザーからの回答を保持する変数
-    let mut ans_intput = String::new();
-
-    // 標準入力から1行取得し、ans_inputに代入する
-    std::io::stdin().read_line(&mut ans_intput).unwrap();
-    let ans_input = ans_intput.trim().parse::<i32>().unwrap();
-    dbg!(ans_input);
-
-    if dbg!(ans_input == op1 - op2) {
-        println!("正解！")
-    } else {
-        println!("不正解！")
-    }
+    println!("クリア！")
 }
