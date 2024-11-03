12757fe01263ceef9b08feb44a3c93a6a1c86b3f
sawady1203 <sawady.dev@gmail.com>
2024-11-03 15:37:54 +0900

    cargo new arithmetric-operations-quiz

---
+++ arithmetric-operations-quiz/target/debug/deps/arithmetric_operations_quiz-333a8d5ebc38683b.d
@@ -0,0 +1,5 @@
+/Users/masayoshi/repo/rust-study/arithmetric-operations-quiz/target/debug/deps/libarithmetric_operations_quiz-333a8d5ebc38683b.rmeta: src/main.rs
+
+/Users/masayoshi/repo/rust-study/arithmetric-operations-quiz/target/debug/deps/arithmetric_operations_quiz-333a8d5ebc38683b.d: src/main.rs
+
+src/main.rs:
---
+++ arithmetric-operations-quiz/target/debug/deps/arithmetric_operations_quiz-b26d315a9825b7a2.d
@@ -0,0 +1,5 @@
+/Users/masayoshi/repo/rust-study/arithmetric-operations-quiz/target/debug/deps/libarithmetric_operations_quiz-b26d315a9825b7a2.rmeta: src/main.rs
+
+/Users/masayoshi/repo/rust-study/arithmetric-operations-quiz/target/debug/deps/arithmetric_operations_quiz-b26d315a9825b7a2.d: src/main.rs
+
+src/main.rs:
