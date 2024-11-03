ec3e76a385b539bc43d8a6c15f82c060998865c1
sawady1203 <sawady.dev@gmail.com>
2024-11-03 15:57:23 +0900

    section/3-3:加算の問題をつくる

--- arithmetric-operations-quiz/src/main.rs
+++ arithmetric-operations-quiz/src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    println!("1 + 1 = ??");
+    println!("?? の値を入兎力してください:");
+    // ユーザーからの回答を保持する変数
+    let mut ans_intput = String::new();
+
+    // 標準入力から1行取得し、ans_inputに代入する
+    std::io::stdin().read_line(&mut ans_intput).unwrap();
+
+    let ans_input = ans_intput.trim().parse::<u32>().unwrap();
+
+    dbg!(ans_input);
+
+    if dbg!(ans_input == 1 + 1) {
+        println!("正解！")
+    } else {
+        println!("不正解！")
+    }
 }
