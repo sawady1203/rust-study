02df878e7a4b8499575507ef5f1a0c468bfd8b24
sawady1203 <sawady.dev@gmail.com>
2024-11-03 17:00:59 +0900

    section:4-4/トランプのカードに変数を代入しよう

--- simple-poker/src/main.rs
+++ simple-poker/src/main.rs
@@ -1,3 +1,22 @@
+// enum型
+#[derive(Debug, Clone, Copy, PartialEq)]
+enum Suit {
+    Club,
+    Diamond,
+    Heart,
+    Spade,
+}
+
+// 構造体
+#[derive(Debug, Clone, Copy, PartialEq)]
+struct Card {
+    suit: Suit,
+    rank: i32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let suit = Suit::Club;
+    let rank = 1;
+    let card = Card { suit, rank };
+    println!("{:?}", card);
 }
