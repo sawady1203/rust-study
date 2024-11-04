072b756c6b528d999516b02c2e39fde667365ae8
sawady1203 <sawady.dev@gmail.com>
2024-11-04 15:31:49 +0900

    section5-3

--- dentaku/src/main.rs
+++ dentaku/src/main.rs
@@ -13,6 +13,15 @@ fn main() {
         let tokens: Vec<&str> = line.split(char::is_whitespace).collect();
         println!("{:?}", tokens);
 
+        // メモリへの書き込み
+        if tokens[0] == "mem+" {
+            add_and_print_memory(&mut memory, prev_result);
+            continue;
+        } else if tokens[0] == "mem-" {
+            add_and_print_memory(&mut memory, -prev_result);
+            continue;
+        }
+
         // 式の計算
         let left: f64 = eval_token(tokens[0], memory);
         let right: f64 = eval_token(tokens[2], memory);
@@ -23,16 +32,17 @@ fn main() {
 
         prev_result = result;
         // prevの表示
-        print_result(prev_result);
+        println!("prev_result: {:}", prev_result);
     }
 }
 
 fn print_result(value: f64) {
-    println!(" => {}", value);
+    println!("result: => {}", value);
 }
 
 fn eval_token(token: &str, memory: f64) -> f64 {
     if token == "mem" {
+        println!("token is mem");
         memory
     } else {
         token.parse().unwrap()
@@ -47,7 +57,13 @@ fn eval_expression(left: f64, operator: &str, right: f64) -> f64 {
         "/" => left / right,
         _ => {
             // 入力が正しいならここにはこない
+            println!("unreachble");
             unreachable!()
         }
     }
+}
+
+fn add_and_print_memory(memory: &mut f64, prev_result: f64) {
+    *memory += prev_result;
+    print_result(*memory);
 }
