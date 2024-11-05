772a19e548c5a472f72fbb626a397b43a102c21b
sawady1203 <sawady.dev@gmail.com>
2024-11-05 21:08:26 +0900

    memに名前をつける

--- dentaku/src/main.rs
+++ dentaku/src/main.rs
@@ -1,7 +1,9 @@
 use std::io::stdin;
 
 fn main() {
-    let mut memories: Vec<f64> = vec![0.0; 10];
+    let mut memory = Memory {
+        slots: vec![0.0; 10],
+    };
     let mut prev_result: f64 = 0.0;
     for line in stdin().lines() {
         // 1行読み取って空行なら終了
@@ -16,17 +18,17 @@ fn main() {
         // メモリへの書き込み
         let is_memory = tokens[0].starts_with("mem");
         if is_memory && tokens[0].ends_with("+") {
-            add_and_print_memory(&mut memories, tokens[0], prev_result);
+            add_and_print_memory(&mut memory, tokens[0], prev_result);
             continue;
         } else if is_memory && tokens[0].ends_with("-") {
-            add_and_print_memory(&mut memories, tokens[0], -prev_result);
+            add_and_print_memory(&mut memory, tokens[0], -prev_result);
             continue;
         }
 
         // 式の計算
         // memoriesを参照渡しにする
-        let left: f64 = eval_token(tokens[0], &memories);
-        let right: f64 = eval_token(tokens[2], &memories);
+        let left: f64 = eval_token(tokens[0], &memory);
+        let right: f64 = eval_token(tokens[2], &memory);
         let result = eval_expression(left, tokens[1], right);
 
         // 結果の表示
@@ -42,10 +44,14 @@ fn print_result(value: f64) {
     println!("result: => {}", value);
 }
 
-fn eval_token(token: &str, memories: &[f64]) -> f64 {
+struct Memory {
+    slots: Vec<f64>,
+}
+
+fn eval_token(token: &str, memory: &Memory) -> f64 {
     if token.starts_with("mem") {
         let slot_index: usize = token[3..].parse().unwrap();
-        memories[slot_index]
+        memory.slots[slot_index]
     } else {
         token.parse().unwrap()
     }
@@ -65,8 +71,8 @@ fn eval_expression(left: f64, operator: &str, right: f64) -> f64 {
     }
 }
 
-fn add_and_print_memory(memories: &mut [f64], token: &str, prev_result: f64) {
+fn add_and_print_memory(memory: &mut Memory, token: &str, prev_result: f64) {
     let slot_index: usize = token[3..token.len() - 1].parse().unwrap();
-    memories[slot_index] += prev_result;
-    print_result(memories[slot_index]);
+    memory.slots[slot_index] += prev_result;
+    print_result(memory.slots[slot_index]);
 }
