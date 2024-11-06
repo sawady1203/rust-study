b1b34666bc836dd470ee5ead513802d226323a4a
sawady1203 <sawady.dev@gmail.com>
2024-11-06 21:13:25 +0900

    Memoryを辞書化

--- dentaku/src/main.rs
+++ dentaku/src/main.rs
@@ -1,10 +1,8 @@
+use std::collections::{hash_map::Entry, HashMap};
 use std::io::stdin;
 
 fn main() {
-    let mut memory = Memory {
-        // 最初、メモリにはなにも記録されていない
-        slots: vec![],
-    };
+    let mut memory = Memory::new();
     let mut prev_result: f64 = 0.0;
     for line in stdin().lines() {
         // 1行読み取って空行なら終了
@@ -19,17 +17,17 @@ fn main() {
         // メモリへの書き込み
         let is_memory = tokens[0].starts_with("mem");
         if is_memory && tokens[0].ends_with("+") {
-            add_and_print_memory(&mut memory, tokens[0], prev_result);
+            memory.add_and_print(tokens[0], prev_result);
             continue;
         } else if is_memory && tokens[0].ends_with("-") {
-            add_and_print_memory(&mut memory, tokens[0], -prev_result);
+            memory.add_and_print(tokens[0], -prev_result);
             continue;
         }
 
         // 式の計算
         // memoriesを参照渡しにする
-        let left: f64 = eval_token(tokens[0], &memory);
-        let right: f64 = eval_token(tokens[2], &memory);
+        let left: f64 = memory.eval_token(tokens[0]);
+        let right: f64 = memory.eval_token(tokens[2]);
         let result = eval_expression(left, tokens[1], right);
 
         // 結果の表示
@@ -44,42 +42,42 @@ fn main() {
 fn print_result(value: f64) {
     println!("result: => {}", value);
 }
-#[derive(Debug)]
+
 struct Memory {
-    // タプル
-    // メモリの名前と値の組を配列で保存する
-    slots: Vec<(String, f64)>,
+    // メモリの名前と値の組を辞書で保存する
+    slots: HashMap<String, f64>,
 }
 
-fn add_and_print_memory(memory: &mut Memory, token: &str, prev_result: f64) {
-    let slot_name = &token[3..token.len() - 1];
-    for slot in memory.slots.iter_mut() {
-        if slot.0 == slot_name {
-            // メモリが見つかったので、値を更新、表示して終了する
-            slot.1 += prev_result;
-            print_result(slot.1);
+impl Memory {
+    fn new() -> Self {
+        Self {
+            slots: HashMap::new(),
         }
     }
-    // メモリが見つからなかったので、最後の要素に追加する
-    memory.slots.push((slot_name.to_string(), prev_result));
-    println!("Memory: {:?}", memory);
-    print_result(prev_result);
-}
-
-fn eval_token(token: &str, memory: &Memory) -> f64 {
-    if token.starts_with("mem") {
-        let slot_name = &token[3..];
-        // すべてのメモリを探索する
-        for slot in &memory.slots {
-            if slot.0 == slot_name {
-                // メモリが見つかったので、値を返して終了
-                return slot.1;
+    fn add_and_print(&mut self, token: &str, prev_result: f64) {
+        let slot_name = &token[3..token.len() - 1].to_string();
+        match self.slots.entry(slot_name.to_string()) {
+            Entry::Occupied(mut entry) => {
+                // メモリが見つかったので、値を更新、表示して終了
+                *entry.get_mut() += prev_result;
+                print_result(*entry.get());
+            }
+            Entry::Vacant(entry) => {
+                // メモリが見つからなかったので、要素を追加する
+                entry.insert(prev_result);
+                print_result(prev_result);
             }
         }
-        // メモリが見つからなかったので、0を返す
-        0.0
-    } else {
-        token.parse().unwrap()
+    }
+
+    fn eval_token(&self, token: &str) -> f64 {
+        if token.starts_with("mem") {
+            let slot_name = &token[3..];
+            // すべてのメモリを探索する
+            self.slots.get(slot_name).copied().unwrap_or(0.0)
+        } else {
+            token.parse().unwrap()
+        }
     }
 }
 
