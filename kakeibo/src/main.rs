35daddbf45c68caecbb6678fd671cf17ef1be9a0
sawady1203 <sawady.dev@gmail.com>
2024-11-09 20:25:06 +0900

    clapを追加

--- kakeibo/src/main.rs
+++ kakeibo/src/main.rs
@@ -1,6 +1,14 @@
+use clap::Parser;
+#[derive(Parser)]
+struct Args {
+    arg1: String,
+    arg2: String,
+}
+
 fn main() {
-    // コマンドラインの引数の一覧を出力する
-    for arg in std::env::args() {
-        println!("{}", arg)
-    }
+    // 構造体Argsで定義した形の引数を受け取ることを期待してparseを行う
+    let _args = Args::parse();
+    let command_name = std::env::args().nth(0).unwrap_or("CLI".to_string());
+    let name = std::env::args().nth(1).unwrap_or("World".to_string());
+    println!("Hello {} via {}!", name, command_name);
 }
