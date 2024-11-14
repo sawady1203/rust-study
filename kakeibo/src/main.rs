e00524262d88ae450f29052a4d7848fdb32615b4
sawady1203 <sawady.dev@gmail.com>
2024-11-14 20:01:20 +0900

    6-2

--- kakeibo/src/main.rs
+++ kakeibo/src/main.rs
@@ -1,14 +1,26 @@
-use clap::Parser;
+use clap::{Parser, Subcommand};
 #[derive(Parser)]
-struct Args {
-    arg1: String,
-    arg2: String,
+#[clap(version = "1.0")]
+struct App {
+    #[clap(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand)]
+enum Command {
+    /// 新しい口座をつくる
+    New,
+    /// 口座に入金する
+    Deposit,
+    /// 口座から出勤する
+    Withdraw,
+    /// CSVからインポートする
+    Import,
+    /// レポートを出力する
+    Report,
 }
 
 fn main() {
     // 構造体Argsで定義した形の引数を受け取ることを期待してparseを行う
-    let _args = Args::parse();
-    let command_name = std::env::args().nth(0).unwrap_or("CLI".to_string());
-    let name = std::env::args().nth(1).unwrap_or("World".to_string());
-    println!("Hello {} via {}!", name, command_name);
+    let _args = App::parse();
 }
