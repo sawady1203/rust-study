1e7ddb79e6d97768ec4aac42b08018d79a20d6c3
sawady1203 <sawady.dev@gmail.com>
2024-11-14 21:04:26 +0900

    Importコマンド

--- kakeibo/src/main.rs
+++ kakeibo/src/main.rs
@@ -1,7 +1,7 @@
-use std::fs::OpenOptions;
-
+use chrono::NaiveDate;
 use clap::{Args, Parser, Subcommand};
-use csv::Writer;
+use csv::{Reader, Writer};
+use std::fs::OpenOptions;
 #[derive(Parser)]
 #[clap(version = "1.0")]
 struct App {
@@ -14,11 +14,11 @@ enum Command {
     /// 新しい口座をつくる
     New(NewArgs),
     /// 口座に入金する
-    Deposit,
+    Deposit(DepositArgs),
     /// 口座から出勤する
-    Withdraw,
+    Withdraw(WithdrawArgs),
     /// CSVからインポートする
-    Import,
+    Import(ImportArgs),
     /// レポートを出力する
     Report,
 }
@@ -36,31 +36,99 @@ impl NewArgs {
     }
 }
 
+#[derive(Args)]
+struct DepositArgs {
+    // 引数の型を定義
+    account_name: String,
+    date: NaiveDate,
+    usage: String,
+    amount: u32,
+}
+
+impl DepositArgs {
+    fn run(&self) {
+        // 追記モードでファイルを開く
+        let open_option = OpenOptions::new()
+            .write(true)
+            .append(true)
+            .open(format!("{}.csv", self.account_name))
+            .unwrap();
+        // open_optionを利用した形でwriterを設定
+        let mut writer = Writer::from_writer(open_option);
+        writer
+            .write_record(&[
+                self.date.format("%Y-%m-%d").to_string(),
+                self.usage.to_string(),
+                self.amount.to_string(),
+            ])
+            .unwrap();
+        writer.flush().unwrap();
+    }
+}
+
+#[derive(Args)]
+struct WithdrawArgs {
+    account_name: String,
+    date: NaiveDate,
+    usage: String,
+    amount: u32,
+}
+
+impl WithdrawArgs {
+    fn run(&self) {
+        // 追記モードでファイルを開く
+        let open_option = OpenOptions::new()
+            .write(true)
+            .append(true)
+            .open(format!("{}.csv", self.account_name))
+            .unwrap();
+        // open_optionを利用した形でwriterを設定
+        let mut writer = Writer::from_writer(open_option);
+        writer
+            .write_record(&[
+                self.date.format("%Y-%m-%d").to_string(),
+                self.usage.to_string(),
+                format!("-{}", self.amount),
+            ])
+            .unwrap();
+        writer.flush().unwrap();
+    }
+}
+
+#[derive(Args)]
+struct ImportArgs {
+    src_file_name: String,
+    dst_account_name: String,
+}
+
+impl ImportArgs {
+    fn run(&self) {
+        // 追記モードでファイルを開く
+        let open_option = OpenOptions::new()
+            .write(true)
+            .append(true)
+            .open(format!("{}.csv", self.dst_account_name))
+            .unwrap();
+        let mut writer = Writer::from_writer(open_option);
+        let mut reader = Reader::from_path(&self.src_file_name).unwrap();
+        for result in reader.records() {
+            // Reader は先頭行をヘッダーとして扱うので
+            // このループは2行目以降で実行する
+            let record = result.unwrap();
+            writer.write_record(&record).unwrap();
+        }
+        writer.flush().unwrap();
+    }
+}
+
 fn main() {
     // 構造体Argsで定義した形の引数を受け取ることを期待してparseを行う
     let args = App::parse();
     match args.command {
         Command::New(args) => args.run(),
-        Command::Deposit => deposit(),   // TODO: ここを実装する
-        Command::Withdraw => withdraw(), // TODO: ここを実装する
-        Command::Import => unreachable!(),
+        Command::Deposit(args) => args.run(),
+        Command::Withdraw(args) => args.run(),
+        Command::Import(args) => args.run(),
         Command::Report => unreachable!(),
     }
-}
-
-fn deposit() {
-    // 追記モードでファイルを開く
-    let open_option = OpenOptions::new()
-        .write(true)
-        .append(true)
-        .open("accounts.csv")
-        .unwrap();
-    // open_optionを利用した形でwriterを設定
-    let mut writer = Writer::from_writer(open_option);
-    writer.write_record(["1", "2", "3"]).unwrap(); // ヘッダーを書き込む
-    writer.flush().unwrap();
-}
-
-fn withdraw() {
-    unimplemented!()
 }
