2858d33c7a9ebac712a4785d70d79f4d2d58b4b6
sawady1203 <sawady.dev@gmail.com>
2024-11-15 20:24:17 +0900

    section7-1

---
+++ my_package/target/debug/deps/my_package-19d2fdb80d20915d.d
@@ -0,0 +1,5 @@
+/Users/masayoshi/repo/rust-study/my_package/target/debug/deps/libmy_package-19d2fdb80d20915d.rmeta: src/lib.rs
+
+/Users/masayoshi/repo/rust-study/my_package/target/debug/deps/my_package-19d2fdb80d20915d.d: src/lib.rs
+
+src/lib.rs:
---
+++ my_package/target/debug/deps/my_package-5d44a9cf8b87bb35.d
@@ -0,0 +1,5 @@
+/Users/masayoshi/repo/rust-study/my_package/target/debug/deps/libmy_package-5d44a9cf8b87bb35.rmeta: src/lib.rs
+
+/Users/masayoshi/repo/rust-study/my_package/target/debug/deps/my_package-5d44a9cf8b87bb35.d: src/lib.rs
+
+src/lib.rs:
