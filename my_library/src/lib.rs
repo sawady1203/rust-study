068cf60e6a8ffcc8c4eab4ff3e43e2cf93729fb8
sawady1203 <sawady.dev@gmail.com>
2024-11-17 20:23:22 +0900

    Bookshelfのメソッドを追加

--- my_library/src/lib.rs
+++ my_library/src/lib.rs
@@ -29,11 +29,29 @@ mod Bookshelf {
         }
 
         // 本を追加するメソッド
+        pub fn add_book(&mut self, book: super::book::Book) {
+            self.books.push(book);
+        }
 
         // タイトルで本を検索するメソッド
+        pub fn search_books(
+            &self, 
+            title_query: &str
+        ) -> Vec<&super::book::Book>{
+            todo!("Implement `Bookshelf::search_books`");
+        }
 
         // 本を本棚から取り出すメソッド
+        pub fn remove_book(
+            &mut self,
+            book: &super::book::Book
+        ) -> Option<super::book::Book>{
+            todo!("Implement `Bookshelf::remove_book`");
+        }
 
         // 本棚の本をすべて取り出すメソッド
+        pub fn take_all_books(&mut self) -> Vec<super::book::Book> {
+            todo!("Implement `Bookshelf::take_all_books`");
+        }
     }
 }
