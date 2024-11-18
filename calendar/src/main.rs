375e1f8929e4d5d02fbea3a2f0536c5efcd1a0ac
sawady1203 <sawady.dev@gmail.com>
2024-11-18 20:35:12 +0900

    jsonファイルを読み込むところまで

--- calendar/src/main.rs
+++ calendar/src/main.rs
@@ -1,3 +1,62 @@
+use chrono::NaiveDateTime;
+use clap::{Parser, Subcommand};
+use serde::{Deserialize, Serialize};
+use std::{
+    fs::File,
+    io::BufReader,
+};
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+struct Schedule {
+    // 予定のID
+    id: u64,
+    // 勉強会の名前
+    subject: String,
+    // 開始時間
+    start: NaiveDateTime,
+    // 終了時間
+    end: NaiveDateTime,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+struct Calendar {
+    schedules: Vec<Schedule>,
+}
+
+const SCHEDULE_FILE: &str = "schedule.json";
+
+#[derive(Parser)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// 予定の一覧表示
+    List,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let options = Cli::parse();
+    match options.command {
+        Commands::List => show_list(),
+    }
+}
+
+fn show_list() {
+    // 予定の読み込み
+    let calendar: Calendar = {
+        let file = File::open(SCHEDULE_FILE).unwrap();
+        let reader = BufReader::new(file);
+        serde_json::from_reader(reader).unwrap()
+    };
+    // 予定の表示
+    println!("ID\tStart\tEND\tSUBJECT");
+    for schedule in calendar.schedules {
+        println!(
+            "{}\t{}\t{}\t{}",
+            schedule.id, schedule.start, schedule.end, schedule.subject,
+        )
+    }
 }
