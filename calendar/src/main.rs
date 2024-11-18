16e81a7dc0ad3906df39a0fc9e62a53c2b55fef0
sawady1203 <sawady.dev@gmail.com>
2024-11-18 20:49:48 +0900

    section8-2

--- calendar/src/main.rs
+++ calendar/src/main.rs
@@ -3,7 +3,7 @@ use clap::{Parser, Subcommand};
 use serde::{Deserialize, Serialize};
 use std::{
     fs::File,
-    io::BufReader,
+    io::{BufReader, BufWriter},
 };
 
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
@@ -35,12 +35,22 @@ struct Cli {
 enum Commands {
     /// 予定の一覧表示
     List,
+    /// 予定の追加
+    Add {
+        /// タイトル
+        subject: String,
+        /// 開始時刻
+        start: NaiveDateTime,
+        /// 終了時刻
+        end: NaiveDateTime,
+    }
 }
 
 fn main() {
     let options = Cli::parse();
     match options.command {
         Commands::List => show_list(),
+        Commands::Add {subject, start, end} => add_schedule(subject, start, end)
     }
 }
 
@@ -59,4 +69,29 @@ fn show_list() {
             schedule.id, schedule.start, schedule.end, schedule.subject,
         )
     }
+}
+
+fn add_schedule(subject: String, start: NaiveDateTime, end: NaiveDateTime) {
+    // 予定の読み込み
+    let mut calendar: Calendar = {
+        let file = File::open(SCHEDULE_FILE).unwrap();
+        let reader = BufReader::new(file);
+        serde_json::from_reader(reader).unwrap()
+    };
+
+    // 予定の作成
+    let id = calendar.schedules.len() as u64;
+    let new_schedule = Schedule{
+        id, subject, start, end
+    };
+    // 予定の追加
+    calendar.schedules.push(new_schedule);
+
+    // 予定の保存
+    {
+        let file = File::create(SCHEDULE_FILE).unwrap();
+        let writer = BufWriter::new(file);
+        serde_json::to_writer(writer, &calendar).unwrap();
+    }
+    println!("予定を追加しました");
 }
