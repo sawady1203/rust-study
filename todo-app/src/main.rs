506898cdb9558561d35d4ef3c82d9621034e5285
sawady1203 <sawady.dev@gmail.com>
2024-11-21 20:49:04 +0900

    hello world

--- todo-app/src/main.rs
+++ todo-app/src/main.rs
@@ -1,9 +1,15 @@
 use actix_web::{App, HttpServer};
+use actix_web::get;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new())
+    HttpServer::new(|| App::new().service(hello))
         .bind(("127.0.0.1", 8080))?
         .run()
         .await
+}
+
+#[get("/hello")]
+async fn hello() -> String{
+    "Hello, world".to_string()
 }
