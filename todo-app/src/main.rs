c8c9f7f9a560e2dd4bddb9b12a2ad004668301c3
sawady1203 <sawady.dev@gmail.com>
2024-11-24 20:35:13 +0900

    todo.htmlにtasksを応答

--- todo-app/src/main.rs
+++ todo-app/src/main.rs
@@ -3,20 +3,11 @@ use actix_web::{get, web};
 use askama::Template;
 use askama_actix::TemplateToResponse;
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(hello))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
-}
-
 #[derive(Template)]
 #[template(path = "hello.html")]
 struct HelloTemplate {
     name: String,
 }
-
 
 #[get("/hello/{name}")]
 async fn hello(name: web::Path<String>) -> HttpResponse{
@@ -24,4 +15,25 @@ async fn hello(name: web::Path<String>) -> HttpResponse{
         name: name.into_inner(),
     };
     hello.to_response()
+}
+
+#[derive(Template)]
+#[template(path = "todo.html")]
+struct ToDoTemplate {
+    tasks: Vec<String>
+}
+
+#[get("/")]
+async fn todo() -> HttpResponse {
+    let tasks = vec!["タスク１".to_string(), "タスク２".to_string(), "タスク３".to_string()];
+    let todo = ToDoTemplate { tasks };
+    todo.to_response()
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(hello).service(todo))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
